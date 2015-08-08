781a595c213834483adf532f8d401a6b9a8d9ee7
sts10 <sschlinkert@gmail.com>
2015-08-08 19:58:38 -0400

    smarter surface model, plus added some robot notes

--- lib/robot.rb
+++ lib/robot.rb
@@ -6,5 +6,10 @@ class Robot
   end
   def choose_slot
     rand(7)
+    # to be smarter about choosing a slot:
+    # 1. Make a 7-long array of each's column current row number
+    # 2. Add one to each of those row numbers to get possible moves
+    # scan all possible moves for this_surface.check_for_consec(3) == 2
+    # if none, then scan all possible moves for this_surface.check_for_consec(3) == 1 to find needed blocks
   end
 end
--- lib/surface.rb
+++ lib/surface.rb
@@ -9,44 +9,50 @@ class Surface
   def to_s
     "#{@row_number}, #{@column_number}"
   end
+
   def check_for_winner
+    self.check_for_consec(4)
+  end
+
+  def check_for_consec(num_consec)
+    num_to_check = num_consec - 1
     puts "checking the surface #{self.to_s} for winners..."
     # check horizonal
     horizontal_array_to_check = []
     7.times do |i|
-      horizontal_array_to_check << @board[@row_number][@column_number+3-i] if @board[@row_number] && @board[@row_number][@column_number+3-i]
+      horizontal_array_to_check << @board[@row_number][@column_number+num_to_check-i] if @board[@row_number] && @board[@row_number][@column_number+num_to_check-i]
     end
 
     vertical_array_to_check = []
     7.times do |i|
-      vertical_array_to_check << @board[@row_number+3-i][@column_number] if @board[@row_number+3-i] && @board[@row_number+3-i][@column_number]
+      vertical_array_to_check << @board[@row_number+num_to_check-i][@column_number] if @board[@row_number+num_to_check-i] && @board[@row_number+num_to_check-i][@column_number]
     end
 
     top_left_to_bottom_right_array_to_check = []
     7.times do |i|
-      top_left_to_bottom_right_array_to_check << @board[@row_number+3-i][@column_number+3-i] if @board[@row_number+3-i] && @board[@row_number+3-i][@column_number+3-i]
+      top_left_to_bottom_right_array_to_check << @board[@row_number+num_to_check-i][@column_number+num_to_check-i] if @board[@row_number+num_to_check-i] && @board[@row_number+num_to_check-i][@column_number+num_to_check-i]
     end
 
     top_right_to_bottom_left_array_to_check = []
     7.times do |i|
-      top_right_to_bottom_left_array_to_check << @board[@row_number-3+i][@column_number+3-i] if @board[@row_number-3+i] && @board[@row_number-3+i][@column_number+3-i]
+      top_right_to_bottom_left_array_to_check << @board[@row_number-num_to_check+i][@column_number+num_to_check-i] if @board[@row_number-num_to_check+i] && @board[@row_number-num_to_check+i][@column_number+num_to_check-i]
     end
     
     all_possible_winners_from_this_move = [] 
     all_possible_winners_from_this_move = [horizontal_array_to_check, vertical_array_to_check,top_left_to_bottom_right_array_to_check, top_right_to_bottom_left_array_to_check]
 
-    result = check_array_of_arrays_for_winner(all_possible_winners_from_this_move)
+    result = check_array_of_arrays_for_winner(all_possible_winners_from_this_move, num_consec)
     #binding.pry
     return result
   end
 
-  def check_array_of_arrays_for_winner(array)
+  def check_array_of_arrays_for_winner(array, num_consec)
     result = false # set this as an assumption before we check
 
     array.each do |row|
       #puts "about to check #{row} for winners"
 
-      result = self.check_single_array_for_winner(row)
+      result = self.check_single_array_for_winner(row, num_consec)
       if result != false
         puts "about to return a winner, thanks to the check_array_of_arrays_for_winner method!"
         break
@@ -55,7 +61,7 @@ class Surface
     return result
   end
 
-  def check_single_array_for_winner(array)
+  def check_single_array_for_winner(array, num_consec)
     consec_ones = 0
     consec_twos = 0
     array.each do |space|
@@ -70,11 +76,11 @@ class Surface
         consec_ones = 0
       end
 
-      if consec_ones == 4
+      if consec_ones == num_consec
         puts "P1 wins!"
         return 1
       end 
-      if consec_twos == 4
+      if consec_twos == num_consec
         puts "P2 wins!"
         return 2
       end
