876c2c3f1fd31d37b6d0ef4ef2f4414a1f24f3fa
sts10 <sschlinkert@gmail.com>
2015-08-24 21:05:23 -0400

    attempt at random sleep and forever play

--- twitter_run.rb
+++ twitter_run.rb
@@ -20,38 +20,38 @@ my_game.present_board
 first_tweet = ["(What! shall I be left alone-- without her?)", "Wait a minute", "I've long wanted to ask you one thing", "Here"].sample
 tweet_to_reply_to = levin.tweet("@kitty_1878 #{first_tweet}") 
 
-
 while true
-  chosen_slot = kitty.choose_slot
-  kitty_move = my_game.move(1, chosen_slot)
-  while kitty_move == false
+  while true
+    chosen_slot = kitty.choose_slot
     kitty_move = my_game.move(1, chosen_slot)
-  end
-  kitty_tweet = kitty.tweet_board(my_game, tweet_to_reply_to.id)
-  tweet_to_reply_to = kitty_tweet
+    while kitty_move == false
+      kitty_move = my_game.move(1, chosen_slot)
+    end
+    kitty_tweet = kitty.tweet_board(my_game, tweet_to_reply_to.id)
+    tweet_to_reply_to = kitty_tweet
 
-  winner = my_game.check_for_winner
-  if winner 
-    puts "Kitty wins"
-    break
-  end
-  # sleep random amount
-  sleep 5
-  chosen_slot = levin.choose_slot
-  levin_move = my_game.move(2, chosen_slot)
-  while levin_move == false
+    winner = my_game.check_for_winner
+    if winner 
+      puts "Kitty wins"
+      break
+    end
+    # sleep random amount
+    sleep (rand(2..20)*60)
+    chosen_slot = levin.choose_slot
     levin_move = my_game.move(2, chosen_slot)
+    while levin_move == false
+      levin_move = my_game.move(2, chosen_slot)
+    end
+
+    tweet_to_reply_to = levin.tweet_board(my_game, tweet_to_reply_to.id)
+
+    winner = my_game.check_for_winner
+    if winner 
+      puts "levin wins"
+      break
+    end
+
+    sleep (rand(1..10)*60)
+    # sleep random amount
   end
-
-  tweet_to_reply_to = levin.tweet_board(my_game, tweet_to_reply_to.id)
-
-  winner = my_game.check_for_winner
-  if winner 
-    puts "levin wins"
-    break
-  end
-
-  # sleep random amount
-  sleep 5
 end
-
